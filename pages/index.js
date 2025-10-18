08342db1799e59de24728c3ef0700d8ea5c59473
Abraão Albuquerque <abraaoalbuquerque@gtel.ufc.br>
2025-10-18 15:31:03 +0200

    adding package-lock.json again and modifing home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Eu te amo dms e quero que você me acompanhe nesse curso</h1>;
+  return <h1>Lorena, eu amo você. Se você me ama dá uma risadinha! </h1>;
 }
 
 export default Home;
