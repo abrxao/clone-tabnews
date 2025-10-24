c363619e428232817cf195e9625b71e6c2502794
Abraão Albuquerque <abraaoalbuquerque@gtel.ufc.br>
2025-10-24 15:46:46 +0200

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(req, response) {
+import database from "../../../../infra/database.js";
+async function status(req, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ msg_crn: "tiamo" });
 }
 
