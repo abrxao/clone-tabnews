ba8cf739c06054ab1c7259280b781e42ef765758
Abraão Albuquerque <abraaoalbuquerque@gtel.ufc.br>
2025-10-24 19:17:37 +0200

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
-import database from "infra/database.js";
+import db from "infra/database.js";
 
-async function status(req, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ msg_crn: "tiamo" });
+async function status(_, response) {
+  const updatedAt = new Date().toISOString();
+
+  const versionResult = await db.query("SHOW server_version");
+  const version = versionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await db.query("SHOW max_connections");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenConnectionsResult = await db.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1`,
+    values: [dbName],
+  });
+
+  const openedConnections = dbOpenConnectionsResult.rows[0].count;
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version,
+        opened_connections: openedConnections,
+        max_connections: parseInt(maxConnections),
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /ap1/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const body = await response.json();
+
+  expect(body.updated_at).toBeDefined();
+
+  const parsedDate = new Date(body.updated_at).toISOString();
+  expect(body.updated_at).toBe(parsedDate);
+
+  expect(body.dependencies.database.max_connections).toBe(100);
+  expect(body.dependencies.database.version).toBe("16.0");
+  expect(body.dependencies.database.opened_connections).toBeGreaterThan(0);
 });
