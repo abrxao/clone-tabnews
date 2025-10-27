e4c0ddf70bc395d2a7312352b4803b16e0091426
abrxao <abrxao@gmail.com>
2025-10-28 00:07:29 +0100

    adds `wait-for-postgres` scripts

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,44 @@
+const { exec } = require("node:child_process");
+const readline = require("node:readline");
+
+function writeLine(msg) {
+  readline.cursorTo(process.stdout, 0);
+  process.stdout.write(msg);
+}
+
+function generateSpinner(iteration) {
+  let loading = "              |";
+  const dashIdx = iteration % (loading.length - 1);
+  const spinner =
+    loading.substring(0, dashIdx) +
+    "-" +
+    loading.substring(dashIdx + 1, loading.length);
+  return spinner;
+}
+
+function checkPostgres(currentCount = 0) {
+  exec(
+    "docker exec postgres-dev pg_isready --host localhost",
+    (error, stdout, stderr) => {
+      const nextCount = currentCount + 1;
+      const isReady = stdout.includes("accepting connections");
+
+      if (isReady) {
+        writeLine("");
+        writeLine("\n🟢 Postgres is ready and accepting connections!\n");
+        return;
+      }
+
+      const spinner = generateSpinner(nextCount);
+      const message = `🟠 Waiting for Postgres to accept connections |${spinner}`;
+      writeLine(message);
+
+      setTimeout(() => {
+        checkPostgres(nextCount);
+      }, 50);
+    },
+  );
+}
+
+process.stdout.write("\n");
+checkPostgres(0);
