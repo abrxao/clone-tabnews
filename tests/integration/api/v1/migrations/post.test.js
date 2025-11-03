d9bdc624c54848cbc96ea8f1ab93f4403fc1780b
abrxao <abrxao@gmail.com>
2025-11-03 21:27:08 +0100

    test: rename all descriptions to use a new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,25 +6,36 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("GET to /ap1/v1/migrations should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST to /ap1/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(201);
+
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+
+      test("For the second time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(200);
+
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(0);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
-
-  const responseBody1 = await response1.json();
-  expect(Array.isArray(responseBody1)).toBe(true);
-  expect(responseBody1.length).toBeGreaterThan(0);
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response2.status).toBe(200);
-
-  const responseBody2 = await response2.json();
-  expect(Array.isArray(responseBody2)).toBe(true);
-  expect(responseBody2.length).toBe(0);
-
-  const migrationsQuery = await database.query("SELECT * from pgmigrations;");
-  expect(migrationsQuery.rows.length).toBe(responseBody1.length);
 });
