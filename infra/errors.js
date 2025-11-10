8674cfcd4b26285402d95e2911ad677ee1e4e3c8
abrxao <abrxao@gmail.com>
2025-11-10 21:08:46 +0100

    feat: add `MethodNotAllowedError` and `ServiceError`

--- infra/errors.js
+++ infra/errors.js
@@ -1,9 +1,48 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("A unexpected Internal Error ocurred", { cause });
     this.name = "InternalServerError";
     this.action = "Contact support";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      status_code: this.statusCode,
+      action: this.action,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Method not allowed to this endpoint");
+    this.name = "MethodNotAllowedError";
+    this.message = "Method not allowed to this endpoint";
+    this.action =
+      "Verify if the method HTTP sended is allowed to this endpoint";
+
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      status_code: this.statusCode,
+      action: this.action,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Indisponible Service at moment.", { cause });
+    this.name = "ServiceError";
+    this.action = "Verify if service is disponible";
+    this.statusCode = 503;
   }
 
   toJSON() {
