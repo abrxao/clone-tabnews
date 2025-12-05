97a2f94d352395c261023e07bf9f88761ba6c46a
abrxao <abrxao@gmail.com>
2025-12-05 01:53:57 +0100

    feat: require `create:user` to access `/api/v1/users`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -2,6 +2,7 @@ import { version as uuidVersion } from "uuid";
 import orchestrator from "tests/orchestrator";
 import user from "models/user";
 import password from "models/password";
+import activation from "models/activation";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -125,6 +126,35 @@ describe("POST to /ap1/v1/users", () => {
         message: "This username is already been used",
         action: "Use another username for this operation",
         status_code: 400,
+      });
+    });
+  });
+  describe("Default user", () => {
+    test("Logged user trying to created new user", async () => {
+      const user1 = await orchestrator.createUser();
+      await activation.activateUserByUserID(user1.id);
+      const user1SessionObj = await orchestrator.createSession(user1.id);
+
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          Cookie: `session_id=${user1SessionObj.token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "from_logged_user",
+          email: "from_logged_user@gmail.com",
+          password: "notStrongValue",
+        }),
+      });
+
+      expect(response.status).toBe(403);
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        name: "ForbiddenError",
+        message: "You're not allowed to do this action",
+        status_code: 403,
+        action: "Verify if user have feature:create:user",
       });
     });
   });
